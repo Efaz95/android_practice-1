9beed790d8a4611f5638c028f7d9e414ae46f20e
Efaz Khan <efazahmed@hotmail.com>
2020-08-04 15:53:43 -0400

    Dice App created

--- app/src/main/java/com/example/dicee/MainActivity.java
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.dicee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton = findViewById(R.id.rollButton);
+
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Dice", "Button has been pressed!!🔥");
+
+                Random randNum = new Random();
+
+                int num1 = randNum.nextInt(6);
+                int num2 = randNum.nextInt(6);
+
+                leftDice.setImageResource(diceArray[num1]);
+                rightDice.setImageResource(diceArray[num2]);
+
+            }
+        });
     }
 }
